c5687cbe98f8b47d501164bb159ffe0101bdadf8
Shayen Thomas <shayen71421@gmail.com>
2026-05-23 12:43:07 +0530

    added accessibilbi menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { ScrollProgress } from "./ScrollProgress";
+import { AccessibilityMenu } from "./accessibility-menu";
 
 interface LayoutProps {
   children: ReactNode;
@@ -15,6 +16,7 @@ export function Layout({ children }: LayoutProps) {
       <main id="main-content" className="flex-1 w-full" tabIndex={-1}>
         {children}
       </main>
+      <AccessibilityMenu />
       <Footer />
     </div>
   );
